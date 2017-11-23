e55bb4375f7f229273dbe21a3aae35c3b5d5f9be
derMitHut <33394811+derMitHut@users.noreply.github.com>
2017-11-23 18:46:52 +0100

    Add files via upload

--- switch.s
+++ switch.s
@@ -1,8 +1,24 @@
 	AREA switch, CODE, readonly
-		
+	EXPORT firstProces
 	EXPORT ctxswitch
 
-ctxswitch
-	; R0 = &spold, R1 = spnew
-	BX LR
-	
+firstProces	PROC
+			mov sp, r0
+            pop { r2-r6 }
+            mov r12, r6 
+            mov r11, r5 
+            mov r10, r4 
+            mov r9, r3 
+			mov r8, r2
+			pop { r4-r7, pc }
+			ENDP
+					
+ctxswitch	PROC
+			;sichern von p1
+			push {r4-r12, lr}
+			;holen von p2
+			mov sp,r1 
+			pop {r4-r12,pc} 
+			bx lr
+			ENDP
+				
